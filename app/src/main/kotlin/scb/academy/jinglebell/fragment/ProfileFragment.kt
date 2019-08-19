e2e976a9693b985f4f3bef37d572e69175a05b00
Monthira Thimachai <monthira.thimachai@scb.co.th>
2019-08-19 17:17:49 +0700

    -final put string to profile detail successfully

--- app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
+++ app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
@@ -13,8 +13,12 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_profile.view.*
-import scb.academy.jinglebell.R
+
 import scb.academy.jinglebell.activity.ProfileActivity
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_profile.*
+import scb.academy.jinglebell.R
 
 
 class ProfileFragment : Fragment(){
@@ -22,22 +26,17 @@ class ProfileFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val _view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        val editText = _view.findViewById(R.id.editText) as EditText
-
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
+        _view.editText.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                /* Write your logic here that will be executed when user taps next button */
                 val intent = Intent(context, ProfileActivity::class.java)
                 intent.putExtra("nickname", editText.text.toString())
                 startActivity(intent)
+
+                handled = true
             }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-
-            }
-
+            handled
         })
 
 
